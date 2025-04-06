1252e20c0119b976adf335e83d2f72b0a7ce1deb
ivan-rog <ivan.romero.develop@gmail.com>
2025-04-06 15:46:27 -0600

    Configure AWS SQS settings and update dependencies in application properties and pom.xml

---
+++ src/main/java/com/localstack/aws/sqs/SQSListener.java
@@ -0,0 +1,15 @@
+package com.localstack.aws.sqs;
+
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SQSListener {
+
+    @SqsListener("localstack-queue")
+    public void receiveMessage(String message) {
+        log.info("Received message: {}", message);
+    }
+}
